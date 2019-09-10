b719d9a40d1236b0b447c176d858e4db5b620d99
lucassp12 <lucas_santos_pereira@hotmail.com>
2019-09-09 22:06:46 -0300

    Model de User

---
+++ src/app/models/User.js
@@ -0,0 +1,20 @@
+import sequelize, { Model, Sequelize } from 'sequelize';
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password: Sequelize.STRING,
+        provider: Sequelize.BOOLEAN,
+      },
+      {
+        sequelize,
+      }
+    );
+    return this;
+  }
+}
+
+export default User;

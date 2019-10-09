ac41570a4c29a8d732d992bbe8c30ceb0f983ad9
lucassp12 <lucas_santos_pereira@hotmail.com>
2019-10-09 19:45:59 -0300

    Criação de migration, model, controller e criação de meetup

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -23,6 +23,10 @@ class User extends Model {
     return this;
   }
 
+  static associate(models) {
+    this.hasMany(models.Meetup);
+  }
+
   checkPassword(password) {
     return bcrypt.compare(password, this.password_hash);
   }
--- src/database/index.js
+++ src/database/index.js
@@ -1,21 +1,31 @@
 import Sequelize from 'sequelize';
 
-import User from '../app/models/User';
-import File from '../app/models/File';
-
 import databaseConfig from '../config/database';
 
-const models = [User, File];
+import User from '../app/models/User';
+import Meetup from '../app/models/Meetup';
+import File from '../app/models/File';
+
+const models = [User, File, Meetup];
 
 class Database {
   constructor() {
+    this.connection = new Sequelize(databaseConfig);
+
     this.init();
+    this.associate();
   }
 
   init() {
-    this.connection = new Sequelize(databaseConfig);
+    models.forEach(model => model.init(this.connection));
+  }
 
-    models.map(model => model.init(this.connection));
+  associate() {
+    models.forEach(model => {
+      if (model.associate) {
+        model.associate(this.connection.models);
+      }
+    });
   }
 }
 

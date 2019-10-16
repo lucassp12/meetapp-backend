224081e889296f2ab66efa80f3a91de16ed912d4
lucassp12 <lucas_santos_pereira@hotmail.com>
2019-10-15 21:20:00 -0300

    Database

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,7 @@
 module.exports = {
+  url: process.env.DATABASE_URL,
   dialect: 'postgres',
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  define: {
-    timestamps: true,
-    underscored: true,
-    underscoredAll: true,
+  dialectOptions: {
+    ssl: true,
   },
 };
